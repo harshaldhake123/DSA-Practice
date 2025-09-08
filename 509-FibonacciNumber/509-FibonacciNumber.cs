a091ce52fbc85e48c716001c619d6a87e2a589bf
Harshal Dhake <harshaldhake123@gmail.com>
2025-09-08 23:08:02 +0530

    File updated at 9/8/2025, 11:08:01 PM

--- 509-FibonacciNumber/509-FibonacciNumber.cs
+++ 509-FibonacciNumber/509-FibonacciNumber.cs
@@ -1,8 +1,23 @@
-// Last updated: 9/8/2025, 11:04:34 PM
+// Last updated: 9/8/2025, 11:08:01 PM
 public class Solution {
     public int Fib(int n) {
+        // Using Recursion
        if(n==0) return 0;
        if(n<2) return 1;
        return Fib(n-1)+Fib(n-2);
+    }
+
+    public int FibIterative(int n) {
+       if(n==0) return 0;
+       if(n<2) return 1;
+       var secondLast=0; //F(0)
+       var last=1; //F(1)
+       var current=0;
+       for(int i=2;i<=n;i++){
+            current=last+secondLast;
+            secondLast=last;
+            last=current;
+       }
+       return current;
     }
 }
