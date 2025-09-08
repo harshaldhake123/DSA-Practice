b783f2ecb7fd212a73b9e5d13594bbcce3607ac3
Harshal Dhake <harshaldhake123@gmail.com>
2025-09-08 23:05:42 +0530

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/version/csharp/509-FibonacciNumber_20250908_230542.cs
@@ -0,0 +1,9 @@
+// Last updated: 9/8/2025, 11:05:42 PM
+public class Solution {
+    public int Fib(int n) {
+        // Using Recursion
+       if(n==0) return 0;
+       if(n<2) return 1;
+       return Fib(n-1)+Fib(n-2);
+    }
+}
