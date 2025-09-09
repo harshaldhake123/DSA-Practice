fb7bb08de25edd0a02a7d79d3d2d4598f71a1531
Harshal Dhake <harshaldhake123@gmail.com>
2025-09-09 16:07:33 +0530

    File updated at 9/9/2025, 4:07:32 PM

--- 3438-FindValidPairofAdjacentDigitsinString/3438-FindValidPairofAdjacentDigitsinString.cs
+++ 3438-FindValidPairofAdjacentDigitsinString/3438-FindValidPairofAdjacentDigitsinString.cs
@@ -1,15 +1,15 @@
-// Last updated: 9/9/2025, 4:05:09 PM
+// Last updated: 9/9/2025, 4:07:32 PM
 public class Solution {
     public string FindValidPair(string s) {
         var map=new Dictionary<char,int>();
-        for(int i=0;i<s.Length;i++){
-            if(map.ContainsKey(s[i])){
-                map[s[i]]++;
-                continue;
+        foreach(var c in s){
+            if(map.ContainsKey(c)){
+                map[c]++;
+            } else{
+                map.Add(c,1);
             }
-            map.Add(s[i],1);
         }
-        // Now I have a frequency map.
+
         for(int i=0;i<s.Length-1;i++){
             var current=s[i]-'0';
             var next=s[i+1]-'0';
