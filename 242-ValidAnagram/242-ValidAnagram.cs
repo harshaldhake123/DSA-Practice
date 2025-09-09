b4edb9cdf494b435bd2f542089659364e9480382
Harshal Dhake <harshaldhake123@gmail.com>
2025-09-09 22:12:51 +0530

    File updated at 9/9/2025, 10:12:52 PM

--- 242-ValidAnagram/242-ValidAnagram.cs
+++ 242-ValidAnagram/242-ValidAnagram.cs
@@ -1,6 +1,25 @@
-// Last updated: 9/9/2025, 10:08:59 PM
+// Last updated: 9/9/2025, 10:12:52 PM
 public class Solution {
     public bool IsAnagram(string s, string t) {
+          if(s.Length!=t.Length){
+            return false;
+        }
+        var arr=new int[26];
+        for(var i=0;i<s.Length;i++){
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
+
+        foreach(var i in arr){
+            if(i > 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //TC: O(N), SC: O(N)
+    public bool IsAnagramUsingHashMap(string s, string t) {
         if(s.Length!=t.Length){
             return false;
         }
@@ -28,5 +47,4 @@ public class Solution {
         }
         return true;
     }
-
 }
