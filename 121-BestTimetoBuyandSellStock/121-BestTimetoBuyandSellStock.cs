097420918393bca18ccd75d51b5b1b2c4f959543
Harshal Dhake <harshaldhake123@gmail.com>
2025-09-09 21:42:33 +0530

    File updated at 9/9/2025, 9:42:33 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.cs
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.cs
@@ -1,19 +1,33 @@
-// Last updated: 9/9/2025, 9:42:04 PM
+// Last updated: 9/9/2025, 9:42:33 PM
 public class Solution {
+
     public int MaxProfit(int[] prices) {
-        if(prices.Length < 2) return 0;
-        var left = 0;
-        var right = 1;
+        if(prices.Length<2){ return 0;}
+        var left=0;
+        var right=1;
         var maxProfit=0;
+
         while(right < prices.Length){
-            if(prices[left] < prices[right]){
-                var profit = prices[right] - prices[left];
-                maxProfit = Math.Max(maxProfit,profit);
-            }
-            else{
+            if(prices[left]>prices[right]){
                 left=right;
+            }else{
+                maxProfit=Math.Max(prices[right]-prices[left],maxProfit);
             }
             right++;
+        }
+        return maxProfit;
+    }
+   
+    //TLE
+    public int MaxProfitBruteForce(int[] prices) {
+        var maxProfit=0;
+        for(int i=0;i<prices.Length-1;i++){
+            var buy=prices[i];
+            for(int j=i+1;j<prices.Length;j++){
+                var sell=prices[j];
+                var profit=sell-buy;
+                maxProfit=Math.Max(maxProfit,profit);
+            }
         }
         return maxProfit;
     }
