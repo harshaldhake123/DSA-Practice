393c3b3536bd1e610ff9916d6a607f89e6df6865
Harshal Dhake <harshaldhake123@gmail.com>
2025-09-13 21:43:52 +0530

    File updated at 9/13/2025, 9:43:51 PM

--- 3541-FindMostFrequentVowelandConsonant/3541-FindMostFrequentVowelandConsonant.cs
+++ 3541-FindMostFrequentVowelandConsonant/3541-FindMostFrequentVowelandConsonant.cs
@@ -1,20 +1,21 @@
-// Last updated: 9/13/2025, 9:40:04 PM
-public class Solution {
-    public int MaxFreqSum(string s) {
-        var map=new Dictionary<char,int>();
-        var vowels=new char[]{'a','e','i','o','u'};
-        var maxConsonents=0;
-        var maxVowels=0;
-        foreach(var c in s){
-            if(!map.ContainsKey(c)) map.Add(c,0);
-            map[c]++;
-            Console.WriteLine(c+"\t"+map[c]);
-                if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
-                    maxVowels=Math.Max(map[c],maxVowels);
-                }else{
-                    maxConsonents=Math.Max(map[c],maxConsonents);
-                }
-            }
-        return maxConsonents+maxVowels;
+// Last updated: 9/13/2025, 9:43:51 PM
+public class Solution 
+{
+    public int MaxFreqSum(string s) 
+    {
+        int maxVowel = 0, maxConsonant = 0;
+        int[] count = new int[26];
+
+        foreach (char c in s) count[c - 'a']++;
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (i == 0 || i == 4 || i == 8 || i == 14 || i == 20)
+                maxVowel = Math.Max(maxVowel, count[i]);
+            else
+                maxConsonant = Math.Max(maxConsonant, count[i]);
+        }
+
+        return maxVowel + maxConsonant;
     }
 }
