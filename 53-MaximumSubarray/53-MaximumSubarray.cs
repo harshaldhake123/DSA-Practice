66a9d2e13bf94ccc0e68a2901f620cecd19f1bb8
Harshal Dhake <harshaldhake123@gmail.com>
2025-09-14 23:31:20 +0530

    File updated at 9/14/2025, 11:31:20 PM

--- 53-MaximumSubarray/53-MaximumSubarray.cs
+++ 53-MaximumSubarray/53-MaximumSubarray.cs
@@ -1,17 +1,28 @@
-// Last updated: 9/14/2025, 11:03:01 PM
+// Last updated: 9/14/2025, 11:31:20 PM
 public class Solution {
     
     public int MaxSubArray(int[] nums) {
         // Kadane's algorithm: do not carry negative burden to the future
+        // Also printing the subarray indexes for followup
         var maxSum=nums[0];
         var sum=0;
-        foreach(var n in nums){
-            sum+=n;
+        int start=0;
+        int maxStart=0;
+        int maxEnd=0;
+        for(var i=0;i<nums.Length;i++){
+            if(sum==0) start=i;
+            sum+=nums[i];
+            if(sum > maxSum){
+                maxSum=sum;
+                maxStart=start;
+                maxEnd= i;
+            }
             maxSum = Math.Max(sum,maxSum);
             if(sum<0){
                 sum=0;
             }
         }
+        Console.WriteLine(maxStart+"\t"+maxEnd);
         return maxSum;
     }
 
