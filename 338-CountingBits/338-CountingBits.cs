3aacf8918e11b35ff773e5ae63cb14479d66b4d4
Harshal Dhake <harshaldhake123@gmail.com>
2025-09-16 21:39:56 +0530

    File updated at 9/16/2025, 9:39:56 PM

--- 338-CountingBits/338-CountingBits.cs
+++ 338-CountingBits/338-CountingBits.cs
@@ -1,6 +1,19 @@
-// Last updated: 9/16/2025, 9:09:33 PM
+// Last updated: 9/16/2025, 9:39:56 PM
 public class Solution {
     public int[] CountBits(int n) {
+        var dp=new int[n+1];
+        var offset=1;
+
+        for(int i=1;i<=n;i++){
+            if(i==offset*2){
+                offset=i;
+            }
+            dp[i]=1+dp[i-offset];
+        }
+        return dp;
+    }
+
+    public int[] CountBitsBruteForce(int n) {
         // brute force O(nlogn_base2)
         var arr=new int[n+1];
         for(int i=0;i<=n;i++){
