7a6baf92ea4c0ea5044374b4cb42060e900a5a53
Harshal Dhake <harshaldhake123@gmail.com>
2025-09-17 23:32:11 +0530

    File updated at 9/17/2025, 11:32:11 PM

--- 48-RotateImage/48-RotateImage.cs
+++ 48-RotateImage/48-RotateImage.cs
@@ -1,6 +1,32 @@
-// Last updated: 9/17/2025, 9:47:55 PM
+// Last updated: 9/17/2025, 11:32:11 PM
 public class Solution {
-    public void Rotate(int[][] matrix) {
+public void Rotate(int[][] matrix) {
+    int left=0,right=matrix.Length-1; // Initialize left, right pointers for the outermost layer of the matrix
+    while(left<right){ // Continue as long as there are layers to rotate, moving inward
+        for(int i=0;i<right-left;i++){ // Iterate over the elements of the current layer's top row
+            int top=left, bottom=right; // Define pointers for the top and bottom rows of the current layer, top is same as left and bottom is same as right because its a square matrix 
+
+            // Store the top-left element of the current cycle of 4 elements to be rotated
+            var topLeft=matrix[top][left+i]; // left+i because each element traverses using the i th iteration that runs for the entirity of the layer's square matrix horizontal length
+
+            // Move the bottom-left element to the top-left position,bottom-i because we move up from bottomleft
+            matrix[top][left+i] = matrix[bottom-i][left];
+
+            // Move the bottom-right element to the bottom-left position right-i because we move left from bottom right
+            matrix[bottom-i][left] = matrix[bottom][right-i];
+
+            // Move the top-right element to the bottom-right position,top+i because we move down from top right
+            matrix[bottom][right-i] = matrix[top+i][right];
+
+            // Move the stored top-left element to the top-right position, from the temp variable
+            matrix[top+i][right]=topLeft;
+        }
+        left++; // Move the left pointer inward to the next layer
+        right--; // Move the right pointer inward to the next layer
+        }
+    }
+
+    public void RotateEasyMethod(int[][] matrix) {
         int n = matrix.Length;
 
         // Transpose the matrix
