7f8857a2ff2538e6223a31d6e6127b9549c40d84
Harshal Dhake <harshaldhake123@gmail.com>
2025-09-24 22:11:44 +0530

    Create 208-ImplementTrie(PrefixTree)

---
+++ 208-ImplementTrie(PrefixTree)/208-ImplementTrie(PrefixTree).cs
@@ -0,0 +1,57 @@
+// Last updated: 9/24/2025, 10:11:44 PM
+public class TrieNode{
+    public Dictionary<char,TrieNode> children=new Dictionary<char,TrieNode>();
+    public bool isEndOfWord=false;
+}
+
+public class Trie {
+    public TrieNode root;
+    public Trie() {
+        root=new TrieNode();
+    }
+    
+    public void Insert(string word) {
+        var current=this.root;
+        foreach(var c in word){
+            if(!current.children.ContainsKey(c)){
+                current.children.Add(c,new TrieNode());
+            }
+            current=current.children[c];
+        }
+        current.isEndOfWord=true;
+    }
+    
+    public bool Search(string word) {
+        var current=this.root;
+        foreach(var c in word){
+            if(!current.children.ContainsKey(c)){
+                return false;
+            }
+            current=current.children[c];
+        }
+        return current.isEndOfWord;
+    }
+    
+    public bool StartsWith(string prefix) {
+        var current=this.root;
+        var exists=false;
+        foreach(var c in prefix){
+            if(current.children.ContainsKey(c)){
+                exists=true;
+                current=current.children[c];
+            }else{
+                exists=false;
+                break;
+            }
+        }
+        return exists;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.Insert(word);
+ * bool param_2 = obj.Search(word);
+ * bool param_3 = obj.StartsWith(prefix);
+ */
