d108bf2d983d85493b0fba629cab54b7372f850e
Harshal Dhake <harshaldhake123@gmail.com>
2025-09-24 22:52:39 +0530

    File updated at 9/24/2025, 10:52:39 PM

--- 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.cs
+++ 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.cs
@@ -1,6 +1,6 @@
-// Last updated: 9/24/2025, 10:46:05 PM
+// Last updated: 9/24/2025, 10:52:39 PM
 public class TrieNode{
-    public Dictionary<char,TrieNode> children=new Dictionary<char,TrieNode>();
+    public TrieNode[] children=new TrieNode[26];
     public bool isEnd=false;
 }
 public class WordDictionary {
@@ -12,10 +12,11 @@ public class WordDictionary {
     public void AddWord(string word) {
         var current=root;
         foreach(var c in word){
-            if(!current.children.ContainsKey(c)){
-                current.children.Add(c,new TrieNode());
+            var index=c-'a';
+            if(current.children[index]==null){
+                current.children[index]=new TrieNode();
             }
-            current=current.children[c];
+            current=current.children[index];
         }
         current.isEnd=true;
     }
@@ -31,17 +32,18 @@ public class WordDictionary {
         var current=root;
         var c = word[i];
         if(c =='.'){
-            foreach(var child in current.children.Values){
-                if(this.Search(word,i+1, child)){
+            foreach(var child in current.children){
+                if(child!=null && this.Search(word,i+1, child)){
                     return true;
                 }
             }
             return false;
         }else{
-            if(!current.children.ContainsKey(c)){
+            var index=c-'a';
+            if(current.children[index] ==null){
                 return false;
             }
-            return Search(word, i+1, current.children[c]);
+            return Search(word, i+1, current.children[index]);
         }
     }
 }
