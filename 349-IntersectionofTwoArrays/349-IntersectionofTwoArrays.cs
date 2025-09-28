ccc0954151304e8316513c7f653144645c7f430b
Harshal Dhake <harshaldhake123@gmail.com>
2025-09-28 21:46:51 +0530

    File updated at 9/28/2025, 9:46:54 PM

--- 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.cs
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.cs
@@ -1,6 +1,38 @@
-// Last updated: 9/28/2025, 9:39:50 PM
+// Last updated: 9/28/2025, 9:46:54 PM
 public class Solution {
     public int[] Intersection(int[] nums1, int[] nums2) {
+        Array.Sort(nums1);
+        Array.Sort(nums2);
+
+        var result = new List<int>();
+        int i = 0, j = 0;
+
+        while (i < nums1.Length && j < nums2.Length) {
+            if (i > 0 && nums1[i] == nums1[i - 1]) {
+                i++;
+                continue;
+            }
+
+            if (j > 0 && nums2[j] == nums2[j - 1]) {
+                j++;
+                continue;
+            }
+
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                result.Add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        return result.ToArray();
+    }
+    
+    //TC: O(M+N), SC:O(M+N)
+    public int[] IntersectionWithSet(int[] nums1, int[] nums2) {
         if (nums1.Length > nums2.Length) {
             return Intersection(nums2, nums1);
         }
