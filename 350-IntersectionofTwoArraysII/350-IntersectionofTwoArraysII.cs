782d393da23f2d45e3d1af5dd5655812d9480940
Harshal Dhake <harshaldhake123@gmail.com>
2025-09-28 21:19:39 +0530

    File updated at 9/28/2025, 9:19:42 PM

--- 350-IntersectionofTwoArraysII/350-IntersectionofTwoArraysII.cs
+++ 350-IntersectionofTwoArraysII/350-IntersectionofTwoArraysII.cs
@@ -1,25 +1,28 @@
-// Last updated: 9/28/2025, 9:13:39 PM
+// Last updated: 9/28/2025, 9:19:42 PM
+using System.Collections.Generic;
+using System.Linq;
+
 public class Solution {
     public int[] Intersect(int[] nums1, int[] nums2) {
-        var map1=new Dictionary<int,int>();
-        var map2=new Dictionary<int,int>();
-        foreach(var n in nums1){
-            if(!map1.ContainsKey(n)) map1.Add(n,0);
-            map1[n]++;
+        // Use a map to count the occurrences in the smaller array
+        // to use less space
+        if (nums1.Length > nums2.Length) {
+            return Intersect(nums2, nums1);
         }
 
-        foreach(var n in nums2){
-            if(!map2.ContainsKey(n)) map2.Add(n,0);
-            map2[n]++;
+        var map = new Dictionary<int, int>();
+        foreach (var num in nums1) {
+            if (!map.ContainsKey(num)) {
+                map.Add(num, 0);
+            }
+            map[num]++;
         }
-        var result=new List<int>();
-        foreach(var kvp in map1){
-            if(map2.ContainsKey(kvp.Key)){
-                var occurances=Math.Min(map1[kvp.Key],map2[kvp.Key]);
-                while(occurances>0){
-                    result.Add(kvp.Key);
-                    occurances--;
-                }
+
+        var result = new List<int>();
+        foreach (var num in nums2) {
+            if (map.ContainsKey(num) && map[num] > 0) {
+                result.Add(num);
+                map[num]--; // Decrement the count for each match in nums2
             }
         }
         return result.ToArray();
