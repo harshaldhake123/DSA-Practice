b4d76a6225e805fd02f7b16efa54dc965af34020
Harshal Dhake <harshaldhake123@gmail.com>
2025-10-07 21:59:28 +0530

    File updated at 10/7/2025, 9:59:28 PM

--- 326-PowerofThree/326-PowerofThree.cs
+++ 326-PowerofThree/326-PowerofThree.cs
@@ -1,14 +1,11 @@
-// Last updated: 10/7/2025, 9:58:12 PM
+// Last updated: 10/7/2025, 9:59:28 PM
 public class Solution {
     public bool IsPowerOfThree(int n) {
-        if(n==1) return true;
-        if(n<3) return false;
-        while(n%3==0){
-            n/=3;
-            if(n==1){
-                return true;
+        if(n>1){
+            while(n%3==0){
+                n/=3;
             }
         }
-        return false;
+        return n==1;
     }
 }
