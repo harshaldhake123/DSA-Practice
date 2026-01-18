22db7862046a2ab725fb115968c2a625f2a97e37
Harshal Dhake <harshaldhake123@gmail.com>
2026-01-18 14:40:58 +0530

    File updated at 1/18/2026, 2:41:00 PM

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.cs
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.cs
@@ -1,19 +1,19 @@
-// Last updated: 9/9/2025, 10:36:28 PM
-public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-        var left=0;
-        var right=nums.Length-1;
-        while(left < right){
-            if(nums[left]+nums[right]==target){
-                return [left+1,right+1];
-            }
-            if(nums[left]+nums[right]<target){
-                left++;
-            }
-            else{
-                right--;
-            }
-        }
-        return [1,1];
-    }
-}
+// Last updated: 1/18/2026, 2:41:00 PM
+1public class Solution {
+2    public int[] TwoSum(int[] nums, int target) {
+3        var left=0;
+4        var right=nums.Length-1;
+5        while(left < right){
+6            if(nums[left]+nums[right]==target){
+7                return [left+1,right+1];
+8            }
+9            if(nums[left]+nums[right]<target){
+10                left++;
+11            }
+12            else{
+13                right--;
+14            }
+15        }
+16        return [1,1];
+17    }
+18}
